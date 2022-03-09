fef636a62bce1cfcbb761ee6a22a160e8830205d
Reece Burch <reecer97@yahoo.com>
2022-03-09 14:46:18 -0600

    separate simulation into function

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ import random
 import visualize
 import multiprocessing
 
-stock_inputs = []
-stock_outputs = []
+stock_ticker = 'AAPL'
 indexes = []
 stock_data = []
 num_of_data_points = 50
@@ -28,48 +27,54 @@ def load_data(symbol):
     return df
 
 
+def sim_network(net, starting_index):
+    money = starting_money
+    position = 0
+    last_buy = 0
+    last_sell = 0
+    profit_pct = 0
+    week_fitness = 1
+    tried_buy = False
+    for day in range(num_of_days_to_sim):
+        made_profit = False
+        day_index = starting_index + day * 24
+        day_data = stock_data.loc[day_index,
+                                    ['1. open', '2. high', '3. low', '4. close']].values.tolist()
+        day_data.append(money)
+        day_data.append(position)
+        net_output = net.activate(day_data)
+        softmax_result = softmax(net_output)
+        class_output = np.argmax(
+            ((softmax_result / np.max(softmax_result)) == 1).astype(int))
+        if class_output == 1 and position > postitions_to_buysell:
+            # Sell
+            position -= postitions_to_buysell
+            last_sell = day_data[0]
+            money += day_data[0] * postitions_to_buysell
+            profit_pct = last_sell / last_buy
+            week_fitness *= profit_pct
+        elif class_output == 2 and day_data[3] * postitions_to_buysell < money:
+            # Buy
+            position = position + postitions_to_buysell
+            last_buy = day_data[3]
+            money -= day_data[3] * postitions_to_buysell
+            tried_buy = True
+    profit = money / starting_money
+    net_fitness = week_fitness * profit
+    if not tried_buy:
+        net_fitness = 0
+    return net_fitness, profit
+
 def calc_fitness(genome, config):
     indexes = random.sample(range(
         num_of_days_to_lookback*24, len(stock_data)-num_of_days_to_sim*24), num_of_data_points)
     fitness = 0
-    tried_buy = False
+    
     net = neat.nn.FeedForwardNetwork.create(genome, config)
     for starting_index in indexes:
-        money = starting_money
-        position = 0
-        last_buy = 0
-        last_sell = 0
-        profit_pct = 0
-        week_fitness = 1
-        for day in range(num_of_days_to_sim):
-            made_profit = False
-            day_index = starting_index + day * 24
-            day_data = stock_data.loc[day_index,
-                                      ['1. open', '2. high', '3. low', '4. close']].values.tolist()
-            day_data.append(money)
-            day_data.append(position)
-            net_output = net.activate(day_data)
-            softmax_result = softmax(net_output)
-            class_output = np.argmax(
-                ((softmax_result / np.max(softmax_result)) == 1).astype(int))
-            if class_output == 1 and position > postitions_to_buysell:
-                # Sell
-                position -= postitions_to_buysell
-                last_sell = day_data[0]
-                money += day_data[0] * postitions_to_buysell
-                profit_pct = last_sell / last_buy
-                week_fitness *= profit_pct
-            elif class_output == 2 and day_data[3] * postitions_to_buysell < money:
-                # Buy
-                position = position + postitions_to_buysell
-                last_buy = day_data[3]
-                money -= day_data[3] * postitions_to_buysell
-                tried_buy = True
-        week_fitness *= money / starting_money
-        fitness += week_fitness
-    # fitness = fitness / len(indexes)
-    if not tried_buy:
-        fitness = 0
+        net_fitness, _ = sim_network(net, starting_index)
+        fitness += net_fitness
+    
     return fitness
 
 
@@ -83,19 +88,22 @@ def run(config_file):
     p = neat.Population(config)
 
     # Add a stdout reporter to show progress in the terminal.
-    p = neat.Checkpointer.restore_checkpoint('checkpoints/stock-checkpoint-39')
+    p = neat.Checkpointer.restore_checkpoint('checkpoints/stock-checkpoint-78')
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
     p.add_reporter(neat.Checkpointer(
-        50, filename_prefix="checkpoints/stock-checkpoint-"))
+        10, filename_prefix="checkpoints/stock-checkpoint-"))
 
     # Run for up to 300 generations.
     pe = neat.ParallelEvaluator(multiprocessing.cpu_count(), calc_fitness)
-    winner = p.run(pe.evaluate, 50)
+    winner = p.run(pe.evaluate, 30)
+    
+    net = neat.nn.FeedForwardNetwork.create(winner, config)
+    _, profit = sim_network(net, len(stock_data)-num_of_days_to_sim*24)
 
     # Display the winning genome.
-    print('\nBest genome:\n{!s}'.format(winner))
+    print(f'\nBest genome Profit({profit}):\n{winner}')
 
     node_names = {-1: 'open', -2: 'high', -
                   3: 'low', -4: 'close', -5: 'money', -6: 'pos', 0: 'hold', 1: 'sell', 2: 'buy'}
@@ -110,5 +118,5 @@ if __name__ == '__main__':
     # current working directory.
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'stock-feedforward')
-    stock_data = load_data('FB')
+    stock_data = load_data(stock_ticker)
     run(config_path)
