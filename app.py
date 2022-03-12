579a530ec05228cdcb0ea4790893fb86c6ad6756
Reece <reecer97@yahoo.com>
2022-03-12 15:33:17 -0600

    changes for better fitness

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 
 import os
-from itertools import repeat
-from functools import partial
 import neat
 from neat.math_util import softmax
 import pandas as pd
@@ -11,71 +9,85 @@ import visualize
 import multiprocessing
 
 stock_ticker = 'AAPL'
-indexes = []
-stock_data = []
-num_of_data_points = 50
+starting_checkpoint = 'AAPL-checkpoint-554'
+stock_data = pd.DataFrame()
+num_of_starting_points = 30
 starting_money = 5000
-num_of_days_to_sim = 14
-num_of_days_to_lookback = 7
+num_of_days_to_sim = 5 * 8
+num_of_days_to_lookback = 0
 postitions_to_buysell = 5
+num_of_generations = 500
 
 
 def load_data(symbol):
     data = pd.read_pickle(os.path.join(local_dir, f'data/{symbol}-intraday'))
-    df = pd.DataFrame(data)
 
-    return df
+    return data
 
 
 def sim_network(net, starting_index):
-    money = starting_money
+    cash = starting_money
     position = 0
     last_buy = 0
     last_sell = 0
     profit_pct = 0
-    week_fitness = 1
+    avg_sell_profit = 0
+    avg_loss = 0
+    num_sells = 0
+    num_loss = 0
     tried_buy = False
+    current_stock_value = 0
     for day in range(num_of_days_to_sim):
-        made_profit = False
-        day_index = starting_index + day * 24
-        day_data = stock_data.loc[day_index,
-                                    ['1. open', '2. high', '3. low', '4. close']].values.tolist()
-        day_data.append(money)
-        day_data.append(position)
+        day_index = starting_index + day
+        day_data = stock_data.iloc[day_index].values.tolist()
+        day_data.extend([cash, position, last_buy, last_sell])
         net_output = net.activate(day_data)
         softmax_result = softmax(net_output)
         class_output = np.argmax(
             ((softmax_result / np.max(softmax_result)) == 1).astype(int))
+        close_price = day_data[3]
         if class_output == 1 and position > postitions_to_buysell:
             # Sell
             position -= postitions_to_buysell
-            last_sell = day_data[0]
-            money += day_data[0] * postitions_to_buysell
+            last_sell = close_price
+            cash += close_price * postitions_to_buysell
             profit_pct = last_sell / last_buy
-            week_fitness *= profit_pct
-        elif class_output == 2 and day_data[3] * postitions_to_buysell < money:
+            avg_sell_profit += profit_pct
+            num_sells += 1
+            if profit_pct < 1:
+                avg_loss += profit_pct
+                num_loss += 1
+        elif class_output == 2 and close_price * postitions_to_buysell < cash:
             # Buy
             position = position + postitions_to_buysell
-            last_buy = day_data[3]
-            money -= day_data[3] * postitions_to_buysell
+            last_buy = close_price
+            cash -= close_price * postitions_to_buysell
             tried_buy = True
-    profit = money / starting_money
-    net_fitness = week_fitness * profit
+        current_stock_value = position * close_price
+    if num_sells > 0:
+        avg_sell_profit /= num_sells
+    if num_loss > 0:
+        avg_loss /= num_loss
+    else:
+        avg_loss = 0.92 # Assume 8% loss
+    overal_profit = (current_stock_value * avg_loss + cash) / starting_money
     if not tried_buy:
-        net_fitness = 0
-    return net_fitness, profit
+        overal_profit = 0
+    return overal_profit, avg_sell_profit
 
 def calc_fitness(genome, config):
-    indexes = random.sample(range(
-        num_of_days_to_lookback*24, len(stock_data)-num_of_days_to_sim*24), num_of_data_points)
+    indexes = random.sample(
+        range(num_of_days_to_lookback, len(stock_data)-num_of_days_to_sim),
+        num_of_starting_points)
+    indexes.append(len(stock_data)-num_of_days_to_sim)
     fitness = 0
     
-    net = neat.nn.FeedForwardNetwork.create(genome, config)
+    net = neat.nn.RecurrentNetwork.create(genome, config)
     for starting_index in indexes:
         net_fitness, _ = sim_network(net, starting_index)
         fitness += net_fitness
     
-    return fitness
+    return fitness / len(indexes)
 
 
 def run(config_file):
@@ -88,28 +100,33 @@ def run(config_file):
     p = neat.Population(config)
 
     # Add a stdout reporter to show progress in the terminal.
-    p = neat.Checkpointer.restore_checkpoint('checkpoints/stock-checkpoint-78')
+    checkpoint_path = os.path.join(os.path.dirname(__file__), f'checkpoints/{starting_checkpoint}')
+    if os.path.exists(checkpoint_path):
+        p = neat.Checkpointer.restore_checkpoint(checkpoint_path)
     p.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
     p.add_reporter(neat.Checkpointer(
-        10, filename_prefix="checkpoints/stock-checkpoint-"))
+        num_of_generations, filename_prefix=f'checkpoints/{stock_ticker}-checkpoint-'))
 
     # Run for up to 300 generations.
     pe = neat.ParallelEvaluator(multiprocessing.cpu_count(), calc_fitness)
-    winner = p.run(pe.evaluate, 30)
+    winner = p.run(pe.evaluate, num_of_generations+1)
     
-    net = neat.nn.FeedForwardNetwork.create(winner, config)
-    _, profit = sim_network(net, len(stock_data)-num_of_days_to_sim*24)
+    net = neat.nn.RecurrentNetwork.create(winner, config)
+    profit, avg_sell_profit = sim_network(net, len(stock_data)-num_of_days_to_sim)
 
     # Display the winning genome.
-    print(f'\nBest genome Profit({profit}):\n{winner}')
+    print(f'\nBest genome Profit({profit}) ASP({avg_sell_profit}):\n{winner}')
 
-    node_names = {-1: 'open', -2: 'high', -
-                  3: 'low', -4: 'close', -5: 'money', -6: 'pos', 0: 'hold', 1: 'sell', 2: 'buy'}
-    visualize.draw_net(config, winner, True, node_names=node_names)
-    visualize.plot_stats(stats, ylog=False, view=True)
-    visualize.plot_species(stats, view=True)
+    node_names = {
+        -1: 'open', -2: 'high', -3: 'low', -4: 'close',
+        -5: 'rsi', -6: 'ema', -7: 'sma', -8: 'obv',
+        -9: 'cash', -10: 'pos', -11: 'last_buy', -12: 'last_sell',
+        0: 'hold', 1: 'sell', 2: 'buy'}
+    visualize.draw_net(config, winner, view=True, node_names=node_names)
+    # visualize.plot_stats(stats, ylog=False, view=False)
+    # visualize.plot_species(stats, view=False)
 
 
 if __name__ == '__main__':
--- download_data.py
+++ download_data.py
@@ -1,4 +1,7 @@
 import os
+import time
+from datetime import date
+from dateutil.relativedelta import relativedelta
 from alpha_vantage.timeseries import TimeSeries
 from alpha_vantage.techindicators import TechIndicators
 from pandas import DataFrame
@@ -22,13 +25,20 @@ for symbol in stock_symbols:
     if not os.path.exists(file_path):
         print(f'Downloading {symbol} intraday data from Aplha Vantage...')
         try:
-            data_price, meta_data = ts.get_intraday(symbol, '60min', 'full')
-            data_rsi, meta_data = ti.get_rsi(symbol, interval='daily')
-            data_sma, meta_data = ti.get_sma(symbol, interval='daily')
+            data_price, _ = ts.get_daily(symbol, outputsize='full')
+            data_rsi, _ = ti.get_rsi(symbol, interval='daily')
+            data_ema, _ = ti.get_ema(symbol, interval='daily', time_period=10)
+            data_sma, _ = ti.get_sma(symbol, interval='daily', time_period=50)
+            data_obv, _ = ti.get_obv(symbol, interval='daily')
             df = DataFrame(data=data_price)
-            daily_df = df.resample('D').first()
-            full_df = daily_df.join(
-                [data_rsi.resample('D').first(), data_sma.resample('D').first()])
+            daily_df = df.resample('D').last()
+            daily_df.dropna(inplace=True)
+            full_df = daily_df.join([
+                data_rsi.resample('D').last(), 
+                data_ema.resample('D').last(), 
+                data_sma.resample('D').last(),
+                data_obv.resample('D').last(),
+            ])
             full_df.drop(columns='5. volume', inplace=True)
             full_df.rename(columns={
                 '1. open': 'open',
@@ -36,8 +46,17 @@ for symbol in stock_symbols:
                 '3. low': 'low',
                 '4. close': 'close',
                 'RSI': 'rsi',
-                'SMA': 'sma'},
+                'EMA': 'ema',
+                'SMA': 'sma',
+                'OBV': 'obv'},
                 inplace=True)
+            full_df.dropna(inplace=True)
+            last_2_years = date.today() - relativedelta( years = +2 )
+            full_df = full_df.loc[last_2_years.strftime('%Y-%m-%d'):date.today().strftime('%Y-%m-%d')]
             full_df.to_pickle(file_path)
+            print(f'Downloaded {full_df.shape[0]} Days of data\n')
+
+            #Wait till api free tier resets
+            time.sleep(60)
         except ValueError as err:
             print(f'Error downloading: {err}')
