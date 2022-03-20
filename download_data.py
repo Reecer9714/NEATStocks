913d550cb0d4dfe1dbc22d43cabd77d613400596
Reece <reecer97@yahoo.com>
2022-03-20 15:12:54 -0500

    move download of stock data to separate function

--- download_data.py
+++ download_data.py
@@ -23,40 +23,44 @@ data_folder = os.path.join(local_dir, 'data')
 if not os.path.exists(data_folder):
     os.makedirs(data_folder)
 
+def download_stock_data(symbol):
+    data_price, _ = ts.get_daily(symbol, outputsize='full')
+    data_rsi, _ = ti.get_rsi(symbol, interval='daily')
+    data_ema, _ = ti.get_ema(symbol, interval='daily', time_period=10)
+    data_sma, _ = ti.get_sma(symbol, interval='daily', time_period=50)
+    data_obv, _ = ti.get_stoch(symbol, interval='daily')
+    df = DataFrame(data=data_price)
+    daily_df = df.resample('D').last()
+    daily_df.dropna(inplace=True)
+    full_df = daily_df.join([
+        data_rsi.resample('D').last(), 
+        data_ema.resample('D').last(), 
+        data_sma.resample('D').last(),
+        data_obv.resample('D').last(),
+    ])
+    full_df.drop(columns='5. volume', inplace=True)
+    full_df.rename(columns={
+        '1. open': 'open',
+        '2. high': 'high',
+        '3. low': 'low',
+        '4. close': 'close',
+        'RSI': 'rsi',
+        'EMA': 'ema',
+        'SMA': 'sma',
+        'SlowD': 'slowd',
+        'SlowK': 'slowk'},
+        inplace=True)
+    full_df.dropna(inplace=True)
+    last_2_years = date.today() - relativedelta( years = +2 )
+    full_df = full_df.loc[last_2_years.strftime('%Y-%m-%d'):date.today().strftime('%Y-%m-%d')]
+    return full_df
+
 for symbol in stock_symbols:
     file_path = os.path.join(data_folder, f'{symbol}-intraday')
     if not os.path.exists(file_path):
         print(f'Downloading {symbol} intraday data from Aplha Vantage...')
         try:
-            data_price, _ = ts.get_daily(symbol, outputsize='full')
-            data_rsi, _ = ti.get_rsi(symbol, interval='daily')
-            data_ema, _ = ti.get_ema(symbol, interval='daily', time_period=10)
-            data_sma, _ = ti.get_sma(symbol, interval='daily', time_period=50)
-            data_obv, _ = ti.get_stoch(symbol, interval='daily')
-            df = DataFrame(data=data_price)
-            daily_df = df.resample('D').last()
-            daily_df.dropna(inplace=True)
-            full_df = daily_df.join([
-                data_rsi.resample('D').last(), 
-                data_ema.resample('D').last(), 
-                data_sma.resample('D').last(),
-                data_obv.resample('D').last(),
-            ])
-            full_df.drop(columns='5. volume', inplace=True)
-            full_df.rename(columns={
-                '1. open': 'open',
-                '2. high': 'high',
-                '3. low': 'low',
-                '4. close': 'close',
-                'RSI': 'rsi',
-                'EMA': 'ema',
-                'SMA': 'sma',
-                'SlowD': 'slowd',
-                'SlowK': 'slowk'},
-                inplace=True)
-            full_df.dropna(inplace=True)
-            last_2_years = date.today() - relativedelta( years = +2 )
-            full_df = full_df.loc[last_2_years.strftime('%Y-%m-%d'):date.today().strftime('%Y-%m-%d')]
+            full_df = download_stock_data(symbol)
             full_df.to_pickle(file_path)
             print(f'Downloaded {full_df.shape[0]} Days of data\n')
 
